4b2ada198e13f0b3c57819931cad8fcd7dbe2984
viennl <viennl@vnext.com.vn>
2020-11-24 01:42:03 +0700

    fix api

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ const User = require('../model/user')
 const Profile = require('../model/user-profile')
 var router = express.Router();
 const auth = require('../middleware/auth')
+const bcrypt = require('bcryptjs')
+var nodemailer = require('nodemailer');
+var handlebars = require('handlebars');
+var fs = require('fs');
 
 // get user profile 
 router.get('/users/me', auth, async (req, res) => {
@@ -56,6 +60,8 @@ router.post('/users', async (req, res) => {
   // Create a new user
   try {
     const { email, password } = req.body;
+    const checkEmail = await User.findOne({email: email})
+    if (checkEmail) res.send({error: 'MSE00029'}) //MSE00029 = 'The email address is already exists.';
     const user = new User({
       email: email,
       password: password,
@@ -90,8 +96,8 @@ router.put('/users/:id', auth, async (req, res) => {
   // Create a new user
   try {
     const checkExist = await User.findById(req.params.id)
-    const { name, password } = req.body;
-    checkExist.name = name
+    const { email, password } = req.body;
+    checkExist.email = email
     checkExist.password = password
     await checkExist.save()
     res.status(200).send(checkExist)
@@ -109,6 +115,83 @@ router.post('/users/me/logout', auth, async (req, res) => {
     })
     await req.user.save()
     res.send()
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+
+// login user 
+router.post('/users/me/login', async (req, res) => {
+  // Log user out of the application
+  try {
+    const user = await User.findOne({email: req.body.email})
+    bcrypt.compare(req.body.password, user.password, function (err, result) {
+      if (result === true) {
+        res.send({role: user.role, token: user.tokens[0].token})
+      } else {
+        throw new Error('MSE00074') // 'The "Email" or "Pasword" is incorrect.'
+      }
+    })
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+// reset password - send mail pass
+router.post('/users/reset/password', async (req, res) => {
+  // Log user out of the application
+  try {
+    const user = await User.findOne({email: req.body.email})
+    console.log(user)
+    const randomstring = Math.random().toString(36).slice(-8);
+    user.password = randomstring;
+
+    var readHTMLFile = function(path, callback) {
+      fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+          if (err) {
+              throw err;
+              callback(err);
+          }
+          else {
+              callback(null, html);
+          }
+      });
+    }
+    // function send mail
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 's2nlvs2@gmail.com',
+        pass: 'Vien09071998#'
+      }
+    });
+
+    await readHTMLFile('mail/resetPass.html', function(err, html) {
+      var template = handlebars.compile(html);
+      var replacements = {
+        passwordReset: randomstring
+      };
+      var htmlToSend = template(replacements);
+
+      var mailOptions = {
+        from: 's2nlvs2@gmail.com',
+        to: user.email,
+        subject: 'FindSafe - RESET PASSWORD',
+        html: htmlToSend
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    });
+
+    await user.save()
+    res.send({user})
   } catch (error) {
     res.status(500).send(error)
   }

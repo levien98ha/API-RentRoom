fff45154628a20c4d8c20689699c23b422dbed63
viennl <viennl@vnext.com.vn>
2020-11-25 23:58:11 +0700

    Update room.js

--- routes/room.js
+++ routes/room.js
@@ -16,32 +16,32 @@ router.get('/room/:id', async (req, res) => {
 // get list room 
 router.get('/room/list', async (req, res) => {
     Room.find({})
-    .skip((req.body.page - 1) * limit)
-    .limit(limit)
-    .exec((err, doc) => {
-        if (err) {
-          return res.json(err);
-        }
-        Room.countDocuments({}).exec((count_error, count) => {
-          if (err) {
-            return res.json(count_error);
-          }
-          return res.json({
-            total: count,
-            page: req.body.page,
-            pageSize: doc.length,
-            data: doc
-          });
+        .skip((req.body.page - 1) * limit)
+        .limit(limit)
+        .exec((err, doc) => {
+            if (err) {
+                return res.json(err);
+            }
+            Room.countDocuments({}).exec((count_error, count) => {
+                if (err) {
+                    return res.json(count_error);
+                }
+                return res.json({
+                    total: count,
+                    page: req.body.page,
+                    pageSize: doc.length,
+                    data: doc
+                });
+            })
         })
-    })
 })
 
 // get recently room
 router.post('/room/recently', async (req, res) => {
     try {
-        Room.find({}).limit(6).sort(['date_time', 1]).exec(function(err, result) {
-            res.status(200).send({result})
-        })   
+        Room.find({}).limit(6).sort(['date_time', 1]).exec(function (err, result) {
+            res.status(200).send({ result })
+        })
     } catch (err) {
         res.status(400).send(error)
     }
@@ -69,31 +69,31 @@ router.post('/room/search', async (req, res) => {
         district: { $regex: '.*' + req.body.district + '.*' },
         ward: { $regex: '.*' + req.body.ward + '.*' }
     })
-    .skip((req.body.page - 1) * limit)
-    .limit(limit)
-    .exec((err, doc) => {
-        if (err) {
-          return res.json(err);
-        }
-        Room.countDocuments({
-            category: req.body.category,
-            price: { $gte: req.body.minPrice, $lte: req.body.maxPrice },
-            are: { $gte: req.body.minArea, $lte: req.body.maxArea },
-            city: { $regex: '.*' + req.body.city + '.*' },
-            district: { $regex: '.*' + req.body.district + '.*' },
-            ward: { $regex: '.*' + req.body.ward + '.*' }
-        }).exec((count_error, count) => {
-          if (err) {
-            return res.json(count_error);
-          }
-          return res.json({
-            total: count,
-            page: req.body.page,
-            pageSize: doc.length,
-            data: doc
-          });
+        .skip((req.body.page - 1) * limit)
+        .limit(limit)
+        .exec((err, doc) => {
+            if (err) {
+                return res.json(err);
+            }
+            Room.countDocuments({
+                category: req.body.category,
+                price: { $gte: req.body.minPrice, $lte: req.body.maxPrice },
+                are: { $gte: req.body.minArea, $lte: req.body.maxArea },
+                city: { $regex: '.*' + req.body.city + '.*' },
+                district: { $regex: '.*' + req.body.district + '.*' },
+                ward: { $regex: '.*' + req.body.ward + '.*' }
+            }).exec((count_error, count) => {
+                if (err) {
+                    return res.json(count_error);
+                }
+                return res.json({
+                    total: count,
+                    page: req.body.page,
+                    pageSize: doc.length,
+                    data: doc
+                });
+            })
         })
-    })
 })
 
 // get list room by customer id 
@@ -102,24 +102,24 @@ router.post('/user/room', async (req, res) => {
     Room.find({
         user_id: req.body.user_id
     })
-    .skip((req.body.page - 1) * limit)
-    .limit(limit)
-    .exec((err, doc) => {
-        if (err) {
-          return res.json(err);
-        }
-        Room.countDocuments({user_id: req.body.user_id}).exec((count_error, count) => {
-          if (err) {
-            return res.json(count_error);
-          }
-          return res.json({
-            total: count,
-            page: req.body.page,
-            pageSize: doc.length,
-            data: doc
-          });
+        .skip((req.body.page - 1) * limit)
+        .limit(limit)
+        .exec((err, doc) => {
+            if (err) {
+                return res.json(err);
+            }
+            Room.countDocuments({ user_id: req.body.user_id }).exec((count_error, count) => {
+                if (err) {
+                    return res.json(count_error);
+                }
+                return res.json({
+                    total: count,
+                    page: req.body.page,
+                    pageSize: doc.length,
+                    data: doc
+                });
+            })
         })
-    })
 })
 
 // create room
@@ -156,41 +156,48 @@ router.post('/room', async (req, res) => {
 router.put('/room', async (req, res) => {
     try {
         const { title, category, photo, status, price, area, time_description, toilet, city, district, ward, description, user_id, user_rent, ex_key } = req.body
-        const roomExist = await Room.findById(req.body.id)
+        const roomExist = await Room.findById(req.body._id)
         const userOwner = await User.findById(req.body.user_id)
         if (((roomExist.user_id !== (await userOwner)._id) && userOwner.role === 'operator') ||
-                userOwner.role === 'enduser') throw new Error('User can not role update room.')
-        roomExist = {
-            title: title,
-            category: category,
-            photo: photo,
-            status: status,
-            price: price,
-            area: area,
-            time_description: time_description,
-            toilet: toilet,
-            city: city,
-            district: district,
-            ward: ward,
-            description: description,
-            user_id: user_id,
-            user_rent: user_rent,
-            date_time: roomExist.date_time,
-            ex_key: ex_key + 1
-        }
-        await room.save()
-
+            userOwner.role === 'enduser') { throw new Error('User can not role update room.') }
+        roomExist.title = title
+        roomExist.category = category
+        roomExist.photo = photo
+        roomExist.status = status
+        roomExist.price = price
+        roomExist.area = area
+        roomExist.time_description = time_description
+        roomExist.toilet = toilet
+        roomExist.city = city
+        roomExist.district = district
+        roomExist.ward = ward
+        roomExist.description = description
+        roomExist.user_id = user_id,
+        roomExist.user_rent = user_rent
+        roomExist.date_time = roomExist.date_time
+        roomExist.ex_key = ex_key + 1
+        await roomExist.save()
         // check list request if room is unavailable
         if (status === 'UNAVAILABLE') {
-            const findRequest = await Request.find({room_id: (await roomExist)._id, status: 'IN PROGRESS'})
+            const findRequest = await Request.find({ room_id: (await roomExist)._id, status: 'IN PROGRESS' })
             if (findRequest) {
                 for (let list in findRequest) {
                     list.status = 'DENIED'
                     await list.save()
                 }
-            } 
+            }
         }
-        res.status(201).send({ room })
+        res.status(201).send(roomExist)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// delete room
+router.delete('room/delete', async (req, res) => {
+    try {
+        Room.findByIdAndDelete(req.body.roomId)
+
     } catch (error) {
         res.status(400).send(error)
     }
@@ -202,9 +209,9 @@ function formatDate() {
         day = '' + d.getDate(),
         year = d.getFullYear();
 
-    if (month.length < 2) 
+    if (month.length < 2)
         month = '0' + month;
-    if (day.length < 2) 
+    if (day.length < 2)
         day = '0' + day;
 
     return [year, month, day].join('-');
